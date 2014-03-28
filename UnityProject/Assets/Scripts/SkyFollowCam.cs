6152d666f0535aa83e6e387833c14ca9dc829ac4
Retl <carlis.moore@gmail.com>
2014-03-28 13:51:26 -0400

    Added some new Scenes. Added 2 Cameraview.
    Maze 1: Original maze.
    Maze 2: Requires dodging more obstacles than the first.
    Maze 3: Same as 2, but in reverse.

---
+++ UnityProject/Assets/Scripts/SkyFollowCam.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyFollowCam : MonoBehaviour
+{
+
+		public GameObject target;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (target != null) {
+						
+						transform.position = new Vector3 (target.transform.position.x, transform.position.y, target.transform.position.z);
+						transform.LookAt (target.transform);
+				}
+		}
+}
